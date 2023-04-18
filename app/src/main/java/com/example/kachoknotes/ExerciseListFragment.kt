02f312cf6125775f6becf257ec90fae668bd6a76
BeanNapkin <margarita@fateev.pro>
2023-04-18 17:03:29 +0300

    Start app. Fragment with list of workouts

--- app/src/main/java/com/example/kachoknotes/ExerciseListFragment.kt
+++ app/src/main/java/com/example/kachoknotes/ExerciseListFragment.kt
@@ -1,12 +1,14 @@
 package com.example.kachoknotes
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kachoknotes.databinding.FragmentExerciseListBinding
+import com.example.kachoknotes.databinding.WorkoutBinding
+import com.example.kachoknotes.entity.Repetition
+import com.example.kachoknotes.entity.Workout
 
 class ExerciseListFragment : Fragment() {
 
@@ -18,23 +20,38 @@ class ExerciseListFragment : Fragment() {
 
     private lateinit var viewModel: ExerciseListViewModel
 
+    val adapter = RecyclerAdapter<Workout>(
+        emptyList(),
+        R.layout.workout
+    ) {workout, _ ->
+        WorkoutBinding.bind(this).apply {
+            nameTextView.text = workout.name
+            repetitionTextView.text = workout.repetitions.first().quantity.toString() + " x " + workout.repetitions.first().weight.toString() + " кг"
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return null
         _binding = FragmentExerciseListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ExerciseListViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val repetition = Repetition(1, 10, 15.00)
+        val repetitions = listOf(repetition)
+
+        val workout = Workout(1, "Приседания со штангой", "", repetitions)
+        val workouts = listOf(workout)
+
+        binding.recyclerView.adapter = adapter
+        adapter.itemList = workouts
     }
 
     companion object {
         fun newInstance() = ExerciseListFragment()
     }
-
 }
---
+++ app/src/main/java/com/example/kachoknotes/entity/Repetition.kt
@@ -0,0 +1,7 @@
+package com.example.kachoknotes.entity
+
+data class Repetition(
+    val id: Int,
+    val quantity: Int,
+    var weight: Double
+)
